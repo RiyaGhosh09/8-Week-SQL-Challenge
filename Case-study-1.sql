e7a5a54b92eb465d4162d6fa0567bb0384763f9a
RiyaGhosh09 <70480555+RiyaGhosh09@users.noreply.github.com>
2023-02-13 22:48:36 +0530

    Update Case-study-1.sql

--- Case-study-1.sql
+++ Case-study-1.sql
@@ -1,58 +1,3 @@
-/*
-CREATE SCHEMA dannys_diner;
-SET search_path = dannys_diner;
-
-CREATE TABLE sales (
-  "customer_id" VARCHAR(1),
-  "order_date" DATE,
-  "product_id" INTEGER
-);
-
-INSERT INTO sales
-  ("customer_id", "order_date", "product_id")
-VALUES
-  ('A', '2021-01-01', '1'),
-  ('A', '2021-01-01', '2'),
-  ('A', '2021-01-07', '2'),
-  ('A', '2021-01-10', '3'),
-  ('A', '2021-01-11', '3'),
-  ('A', '2021-01-11', '3'),
-  ('B', '2021-01-01', '2'),
-  ('B', '2021-01-02', '2'),
-  ('B', '2021-01-04', '1'),
-  ('B', '2021-01-11', '1'),
-  ('B', '2021-01-16', '3'),
-  ('B', '2021-02-01', '3'),
-  ('C', '2021-01-01', '3'),
-  ('C', '2021-01-01', '3'),
-  ('C', '2021-01-07', '3');
- 
-
-CREATE TABLE menu (
-  "product_id" INTEGER,
-  "product_name" VARCHAR(5),
-  "price" INTEGER
-);
-
-INSERT INTO menu
-  ("product_id", "product_name", "price")
-VALUES
-  ('1', 'sushi', '10'),
-  ('2', 'curry', '15'),
-  ('3', 'ramen', '12');
-  
-
-CREATE TABLE members (
-  "customer_id" VARCHAR(1),
-  "join_date" DATE
-);
-
-INSERT INTO members
-  ("customer_id", "join_date")
-VALUES
-  ('A', '2021-01-07'),
-  ('B', '2021-01-09');
-*/
 
 /* --------------------
    Case Study Questions
@@ -64,9 +9,11 @@ select customer_id,sum(price) as total_amount
 from sales s join menu me on s.product_id = me.product_id
 group by 1 order by 1
 
+
 -- 2. How many days has each customer visited the restaurant?
 
 select customer_id, count(distinct(order_date)) from sales group by 1
+
 
 -- 3. What was the first item from the menu purchased by each customer?
 
@@ -93,6 +40,7 @@ from sales s join menu m on s.product_id=m.product_id
 group by 1,2
 order by 3 desc
 limit 1
+
 
 -- 5. Which item was the most popular for each customer?
 
@@ -136,6 +84,7 @@ from members m join sales s on m.customer_id=s.customer_id
 join menu me on me.product_id = s.product_id
 where order_date < join_date
 group by 1
+
 
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
 
